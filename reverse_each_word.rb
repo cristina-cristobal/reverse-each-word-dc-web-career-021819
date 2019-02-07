9de7997b72efbafc702d6ac647c50d75b62c3d8b
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-07 18:43:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,6 @@
 def reverse_each_word(sentence)
-  array = []
-    sentence.each do
-      array << sentence.split
-    end
-    array.each do |word|
-      word.reverse
-    end
-  end 
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect do |word|
+    word.reverse 
+end 
